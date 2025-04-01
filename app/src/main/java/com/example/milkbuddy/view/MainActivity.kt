3c6459b9cd6363159d9ea0001f4fa7b2472bf804
Kushagra <kushagra22.aug@gmail.com>
2025-04-01 17:55:28 +0530

    MVVM architecture added

--- app/src/main/java/com/example/milkbuddy/view/MainActivity.kt
+++ app/src/main/java/com/example/milkbuddy/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.milkbuddy
+package com.example.milkbuddy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.milkbuddy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
